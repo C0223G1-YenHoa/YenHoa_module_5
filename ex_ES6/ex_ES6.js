87cc8719c83d94b86e3352cbda774a82d66da097
C0223G1-YenHoa <lysh31@gmail.com>
2023-07-17 11:16:13 +0700

    em nop bai tap ngay 17/07

---
+++ ex_ES6/ex_ES6.js
@@ -0,0 +1,60 @@
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+
+let newCourses = courses.filter((course) =>
+    course.rating >= 4
+);
+
+console.log(newCourses);
+
+
+console.log((courses.filter(course =>
+    course.rating < 4)).map((c) => c.id + '-' + c.title + '-' + c.rating));
+;
+
+
+let addedCourses = [
+    {
+        id: 6,
+        title: "PHP Tutorial",
+        rating: 3,
+    },
+    {
+        id: 7,
+        title: "C# Tutorial",
+        rating: 2,
+    },
+    {
+        id: 8,
+        title: "Docker Tutorial",
+        rating: 3.8,
+    }
+];
+
+
+console.log(...courses, ...addedCourses);
