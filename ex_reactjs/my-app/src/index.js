7bf1217adb836c4bf94d94ec32ef246a217dc318
C0223G1-YenHoa <lysh31@gmail.com>
2023-07-20 05:38:04 +0700

    em da fix

--- ex_reactjs/my-app/src/index.js
+++ ex_reactjs/my-app/src/index.js
@@ -6,31 +6,37 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 const students = [
   {
+    id: 1,
     company: 'Alfreds Futterkiste',
     contact: 'Maria Anders',
     country: 'Germany'
   },
   {
+    id: 2,
     company: 'Centro comercial Moctezuma',
     contact: 'Francisco Chang',
     country: 'Mexico'
   },
   {
+    id: 3,
     company: 'Ernst Handel',
     contact: 'Roland Mendel',
     country: 'Austria'
   },
   {
+    id: 4,
     company: 'Island Trading',
     contact: 'Helen Bennett',
     country: 'UK'
   },
   {
+    id: 5,
     company: 'Laughing Bacchus Winecellars',
     contact: 'Yoshi Tannamuri',
     country: 'Canada'
   },
   {
+    id: 6,
     company: 'Magazzini Alimentari Riuniti',
     contact: 'Giovanni Rovelli',
     country: 'Italy'
@@ -39,21 +45,25 @@ const students = [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
+  <>
     <h1>STUDENTS</h1>
     <table id="customers">
-      <tr>
-        <th>COMPANY</th>
-        <th>CONTACT</th>
-        <th>COUNTRY</th>
-      </tr>
-      {students.map((student) => (
+      <thead>
         <tr>
-          <td>{student.company}</td>
-          <td>{student.contact}</td>
-          <td>{student.country}</td>
+          <th>COMPANY</th>
+          <th>CONTACT</th>
+          <th>COUNTRY</th>
         </tr>
-      ))};
+      </thead>
+      <tbody>
+        {students.map((student) => (
+          <tr key={student.id}>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))};
+      </tbody>
     </table>
-  </div>
+  </>
 )
